91615732b6dd12afd3650ce5217529174ab27fb2
idohiro <idohiro0509@gmail.com>
2021-08-15 14:55:36 +0000

    indexページとshow ページ編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,37 @@ class BooksController < ApplicationController
   end
 
   def show
+      @book = Book.find(params[:id])
   end
 
   def new
+      @book = Book.new
+  end
+
+  def create
+    book = Book.index(book_params)
+    book.save
+    redirect_to book_path(book_id)
   end
 
   def edit
+        @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Bool.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   def destory
   end
+
+  private
+   def book_params
+    params.require(:book).permit(:title, :body)
+   end
+
+
 
 end
