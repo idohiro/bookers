c233767650344e2952a62454f2db5c0d84b2af21
idohiro <idohiro0509@gmail.com>
2021-08-28 12:31:49 +0000

    完成版

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,21 +27,20 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-     if book.save
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+     if @book.save
          flash[:notice] = 'Book was successfully updated.'
-             redirect_to book_path(book)
+             redirect_to book_path(@book)
     else
-      @books = Book.all
       render:edit
   end
   end
 
-  def destory
+  def destroy
      book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   private
